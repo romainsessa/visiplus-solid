d393033e8f8454c37b8cc46d03e91f6eeb90fd21
Alan <visiplus@visiplus.fr>
2022-11-07 14:28:07 +0100

    change value for employee

--- 1-srp/invalid-srp/src/main/java/fr/visiplus/invalidsrp/Employee.java
+++ 1-srp/invalid-srp/src/main/java/fr/visiplus/invalidsrp/Employee.java
@@ -7,7 +7,7 @@ public class Employee {
 
 	public static Logger logger = LoggerFactory.getLogger(Employee.class);
 	
-	private int daysPerWeek = 5;
+	private int daysPerWeek = 6;
 	//private int workedHoursPerDay = 8; add it after
 	private int hoursPerDay = 7;
 	
